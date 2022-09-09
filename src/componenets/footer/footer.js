6d629fd1f2930c5b693f2b76b2c395dd9282d3e4
Nada <mac@macs-MacBook-Pro.local>
2022-09-09 09:07:03 +0300

    style footer

--- src/componenets/footer/footer.js
+++ src/componenets/footer/footer.js
@@ -1,18 +1,33 @@
-import React from 'react'
-import "./footer.css"
+import React from "react";
+import "./footer.css";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { RiFacebookFill } from "react-icons/ri";
 
 const Footer = () => {
   return (
-   <>
-   <div className='container_footer'>
-    <div className='container_content_footer'>
-        
+    <>
+      <div className="container_footer">
+        <div className="container_content_footer">
+          <div className="LOGO_div">LOGO</div>
+          <div className="List_div">
+            <a className="" href=""> HOME</a>
+            <a href=""> HOME</a>
+            <a href=""> HOME</a>
 
-    </div>
+            {/* <ul className='List_div'>
+          <li>h1</li>
+          <li>h1</li>
+          <li>h1</li>
+        </ul> */}
+          </div>
+          <div className="socialMedia_div">
+            <RiFacebookFill />
+            <AiOutlineInstagram />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-   </div>
-   </>
-  )
-}
-
-export default Footer
+export default Footer;
