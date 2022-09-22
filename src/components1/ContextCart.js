cd9cd7e9873777dea4abb2db947e05716e7fcb00
Ibrahim <ibrahim.nadar20@gmail.com>
2022-09-22 11:36:21 +0300

    shadow aboutus

--- src/components1/ContextCart.js
+++ src/components1/ContextCart.js
@@ -51,7 +51,8 @@ const Cart = () => {
   }
 
   return (
-    <>
+    < >
+
       {/* <Navbar /> */}
 
       <section className="main-cart-section">
@@ -85,71 +86,6 @@ const Cart = () => {
   );
 };
 
+
+
 export default Cart;
-
-// import React, { useContext } from "react";
-// import { Scrollbars } from "react-custom-scrollbars-2";
-// import Items from "./Items";
-// import { CartContext } from "./Cart";
-// import Navbar from "../components/NavBar";
-// const ContextCart = () => {
-//   const { item, clearCart, totalItem, totalAmount } = useContext(CartContext);
-//   console.log(item);
-//   let cart = JSON.parse(localStorage.getItem("cart_products")) || [];
-
-//   if (item.length === 0) {
-//     return (
-//       <div className="body1">
-//         <header className="header">
-//           <div className="continue-shopping">
-//             <img src="./images/arrow.png" alt="arrow" className="arrow-icon" />
-//             <h3>continue shopping</h3>
-//           </div>
-//         </header>
-
-//         <section className="main-cart-section">
-//           <h1>shopping Cart</h1>
-//           <p className="total-items">
-//             you have{" "}
-//             <span className="total-items-count">
-//               {localStorage.getItem("cartNumbers")}{" "}
-//             </span>{" "}
-//             items in shopping cart
-//           </p>
-//         </section>
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <div className="cart_summary">
-//       <Navbar />
-
-//       <section className="main-cart-section">
-//         <div className="cart-desc">
-//           <h3 className="box">
-//             Cart Total : <span>{totalAmount}$</span>
-//           </h3>
-
-//           <p className="box">
-//             you have
-//             <span>{localStorage.getItem("cartNumbers").totalItem} </span> items
-//             in shopping cart
-//           </p>
-//         </div>
-//         <div className="cart-items">
-//           <div className="cart-items-container">
-//             <Scrollbars>
-//               {item.map((curItem) => {
-//                 return <Items key={curItem.id} {...curItem} />;
-//               })}
-//             </Scrollbars>
-//           </div>
-//           <button className="box-check">CheckOut</button>
-//         </div>
-//       </section>
-//     </div>
-//   );
-// };
-
-// export default ContextCart;
